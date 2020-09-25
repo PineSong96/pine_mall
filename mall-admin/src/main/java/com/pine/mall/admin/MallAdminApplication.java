72a76704b68527f55877aeab30b0e4d01c9b4310
Pine <771190883@qq.com>
2020-09-25 21:48:49 +0800

    添加admin模块

---
+++ mall-admin/src/main/java/com/pine/mall/admin/MallAdminApplication.java
@@ -0,0 +1,20 @@
+package com.pine.mall.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @vlog: My code, I understand
+ * @desc: 用户管理后台
+ * @author: pine
+ * @createDate: 2020/9/25
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class MallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+
+}
